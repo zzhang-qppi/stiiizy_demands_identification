7a06e0da56c9af750c9ef02ed829a96d2f95470a
Zhiyu Zhang <zzhang.qppi@gmail.com>
2023-09-25 08:12:01 -0500

    Multiple comments processing

--- label_comment.py
+++ label_comment.py
@@ -3,10 +3,12 @@ import numpy as np
 import openai
 from retry import retry
 import time
-
+import os
+import logging
+from datetime import datetime
 
 # 获取回答
-@retry(Exception, tries=20, delay=5)
+# @retry(exceptions=Exception, tries=20, delay=5)
 def get_response_from_list(m_comment, m_questions):
     # comment: a string of a single comment
     # questions: a list of questions about the comment to be fed to GPT
@@ -15,9 +17,21 @@ def get_response_from_list(m_comment, m_questions):
     #    It is very important that you answer the questions purely based on the content of the comment.
     #    Don't make any interpretation beyond the exact words in the comment. Answer in yes and no only.
     #    '''
-    sys_message = f'''You evaluate this comment '{m_comment}' against a list of {len(m_questions)} questions. 
-    I need you {m_comment} answers in a list indexed by numbers and separated by one new line character. 
-    Give me one and only one answer for each question! Answer in yes and no only.'''
+
+    # sys_message = f'''You evaluate this consumer's comment about a e-cigarette product against a list of {len(m_questions)} criteria. The comment is "{m_comment}". You take
+    # each criterion from the list and answer a question that take a form like this: Does this comment mention [the criterion]?
+    # I need {len(m_questions)} answers in a list indexed by numbers in the exact same order as the criteria list and separated by one new line character.
+    # You should give me one and only one answer for each criterion! Give me the answers in yes/no only. Don't give me the questions.'''
+
+    comment_string = '\n'.join(
+        [f"{chr(97+i)}. " + m_comment.iloc[i] for i in range(len(m_comment))]
+    )
+    sys_message = f'''You evaluate {len(m_comment)} consumer's comments about a e-cigarette product against a list of {len(m_questions)} criteria. The comments are \n{comment_string}.
+    For each comment, you take each criterion from the list and answer a question that take a form like this: Does this comment mention [the criterion]?
+    I need {len(m_questions)} answers for each comment in a list indexed by numbers in the exact same order as the criteria list and separated by one new line character.
+    Since there are {len(m_comment)} comments, you give me {len(m_comment)} lists separated by five dashes '-'.
+    You should give me one and only one answer for each comment and criterion! Give me the answers in yes/no only. Don't give me the questions.
+    Here are the {len(m_questions)} criteria:'''
 
     prompt = '\n'.join(
         [f"{i+1}. " + m_questions.iloc[i] for i in range(len(m_questions))]
@@ -30,6 +44,7 @@ def get_response_from_list(m_comment, m_questions):
         ],
         model="gpt-4",
         temperature=0,
+        request_timeout=60,
     )
 
 
@@ -43,7 +58,8 @@ def comment_labeling_with_gpt(m_comments, m_questions):
     # divide one Series of questions into several smaller Series
     # of questions to comply with the 4096 token limit
     batched_questions = []
-    numb = 6  # number of batches
+    numb = 4
+    # number of batches (which would likely be numb + 1)
     for i in range(numb):
         batched_questions.append(
             m_questions[
@@ -58,7 +74,7 @@ def comment_labeling_with_gpt(m_comments, m_questions):
     try:
         for c in range(len(m_comments)):
             comment = m_comments.iloc[c]
-            answers = pd.DataFrame("//", index=m_questions.index, columns=[comment])
+            answers = pd.DataFrame("//", columns=m_questions.index, index=[comment])
             for j in range(len(batched_questions)):
                 # 将每个问题上传给ChatGPT并获取回答
                 this_batch = batched_questions[j]
@@ -73,22 +89,22 @@ def comment_labeling_with_gpt(m_comments, m_questions):
                     print("wrong number of responses")
                     print(len(list_of_resp), " responses")
                     print(len(this_batch), " questions")
-                answers.loc[
-                    this_batch.index[: len(list_of_resp)], comment
-                ] = list_of_resp
+                answers.loc[comment, this_batch.index[: len(list_of_resp)]] = list_of_resp
                 print(
-                    f"{counter}; {m_comments.index[c]}th comment; {j}th batch"
+                    f"{counter}; {m_comments.index[c]}th comment; {j}th batch; {datetime.now()}"
                 )  # counter提示程序正常运行
                 counter += 1
             all_answers.append(answers.copy())
+            print(c, 'comment appended.')
     except Exception as err:
         # 如果出错，保存现有的回答，防止已有进度丢失
         print(err)
     finally:
-        all_answers_df = pd.concat((all_answers), axis=1)
-        return all_answers_df.applymap(
-            lambda x: x.strip("\n") if type(x) == str else x
-        )  # strip the extra \n
+        return pd.concat(all_answers)
+#         all_answers_df = pd.concat(all_answers)
+#         return all_answers_df.applymap(
+#             lambda x: x.strip("\n") if type(x) == str else x
+#         )  # strip the extra \n
 
 
 def refill_na_values(m_results, m_questions):
@@ -98,7 +114,7 @@ def refill_na_values(m_results, m_questions):
         assert len(m_questions) == len(m_results.index)
         m_comments = m_results.columns
         naloc = list(
-            zip(*np.where(m_results.isnull()))
+            zip(*np.where(m_results == '//'))
         )  # a list of integar locations of nan values; [(row, column),]
         print(len(naloc), "nan values are found.")
         for loc in naloc:
@@ -117,21 +133,47 @@ def refill_na_values(m_results, m_questions):
         return m_results
 
 
-start = time.time()
+def main(comment_file_name, read_directory, save_directory, questions_directory, api_key):
+    start = time.time()
+    g_labels_questions = pd.read_csv(questions_directory, index_col=0)
+    g_questions = g_labels_questions["criterion"]
+    save_path = os.path.join(save_directory, comment_file_name)
+    read_path = os.path.join(read_directory, comment_file_name)
 
-g_labels_questions = pd.read_csv("secondary_labels_questions.csv", index_col=0)
-g_questions = g_labels_questions["questions"]
-g_comments = pd.read_csv("xros3mini_comments.csv", index_col=0).iloc[:2, 0]
-openai.api_key = "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"  # ChatGPT密匙
+    # Create result file and fill in the header if there isn't one.
+    if os.path.isfile(save_path):
+        old_results = pd.read_csv(save_path, index_col=0)
+        if len(old_results) == 0:
+            last_comment = 0
+        else:
+            last_comment = len(old_results)
+    else:
+        pd.DataFrame(columns=g_questions).to_csv(save_path)
+        last_comment = 0
 
-new_results = comment_labeling_with_gpt(g_comments, g_questions)
-old_results = pd.read_csv('results/xros3mini_secondary_test_responses.csv', index_col=0)
-refill_na_values(
-    new_results, g_questions
-)  # Feed questions with nan results to GPT again
+    # Continue reading comments from the last one unprocessed
+    g_comments = pd.read_csv(read_path, index_col=0)['comment'].dropna().loc[last_comment:]
 
-results = pd.concat((old_results, new_results), axis=1)  # Concatenate the old and new results
-results.to_csv("results/xros3mini_secondary_test_responses.csv")
-print(
-    (time.time() - start) / len(new_results.index) / len(new_results.columns)
-)  # Print average processing time
+    print(g_comments)
+    openai.api_key = api_key  # ChatGPT密匙
+    new_results = comment_labeling_with_gpt(g_comments, g_questions)
+    refill_na_values(
+        new_results, g_questions
+    )  # Feed questions with nan results to GPT again
+
+    new_results.to_csv(os.path.join(save_directory, comment_file_name), header=False, mode='a')
+    print(
+        (time.time() - start) / len(new_results.index) / len(new_results.columns)
+    )  # Print average processing time
+
+
+# 运行语句
+# if __name__ == '__main__':
+#     logging.basicConfig(filename='mylog.log', encoding='utf-8', level=logging.INFO, filemode='a', format='%(asctime)s %(message)s')
+#     main('weedmaps.csv', 'data', 'processed-results', 'label_and_questions.csv', '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')
+
+g_labels_questions = pd.read_csv('label_and_questions.csv', index_col=0)['criterion']
+g_comments = pd.read_csv('data/weedmaps.csv', index_col=0)['comment'].dropna()
+openai.api_key='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'
+print(get_response_from_list(g_comments[11:15], g_labels_questions[:20])["choices"][0]["message"]["content"])
+
