da303c26c6491ca9c97c39751d5692177a959224
Zhiyu Zhang <zzhang.qppi@gmail.com>
2023-10-03 21:55:43 -0500

    Bugs fixed. Making a generator.

--- label_comment_2.py
+++ label_comment_2.py
@@ -10,8 +10,8 @@ from datetime import datetime
 
 
 # 获取回答
-# @retry(exceptions=Exception, tries=20, delay=5)
-def get_response_from_gpt(m_comment, m_criteria):
+@retry(exceptions=Exception, tries=10, delay=5)  # 若出错，重试20次，每次暂停5秒
+def get_response_from_gpt(m_comments, m_criteria):
     # comment: a string of a single comment
     # questions: a list of questions about the comment to be fed to GPT
 
@@ -28,58 +28,66 @@ def get_response_from_gpt(m_comment, m_criteria):
     # You should give me one and only one answer for each criterion! Give me the answers in yes/no only.
     # Don't give me the questions.'''
 
-    if type(m_comment) is str:
-        indexed_comment_string = m_comment
+    if type(m_comments) is str:
+        indexed_comment_string = m_comments
     else:
-        indexed_comment_string = '\n'.join(
-            [f"{num_to_letter_index(i)}. " + m_comment[i] for i in m_comment.index]
+        indexed_comment_string = "comment's index | comment\n----|----------\n" + '\n'.join(
+            [f"{num_to_letter_index(i)} | '" + m_comments[i] + "'" for i in m_comments.index]
         )
+    indexed_criteria_string = "criterion's index | criterion\n----|------\n" + '\n'.join(
+        [f"{i} | " + m_criteria[i] for i in m_criteria.index]
+    )
     # prompt = f'''Here is a buyer's comment of e-cigarette: "{indexed_comment_string}".
     # Which ones of the following criteria does this comment mention? Answer simply with the numerical indices.
     # \n'''
-    prompt = f'''Here are a list of buyer's comments on an e-cigarette product:\n"{indexed_comment_string}". 
-    In the exact order, take one comment at a time and answer:
-    Which ones of the following criteria does this comment mention? 
-    Answer simply with the numerical indices!
-    If the comment doesn't mention any of the criteria, answer -1 for that answer.
-    Your response should compose of lines in the format of this:
-    [index of a comment] ***: [a list of indices of the mentioned criteria]
-    \n'''
+    prompt = f'''Here is a list of buyer's comments on an e-cigarette product:\n"{indexed_comment_string}" 
 
-    indexed_criteria_string = '\n'.join(
-        [f"{i}. " + m_criteria[i] for i in m_criteria.index]
-    )
-    print(prompt+indexed_criteria_string)
+In the exact order, take one comment at a time and answer:
+Which ones of the following criteria does this comment mention? 
+Answer simply with the numerical indices!
+If the comment doesn't mention any of the criteria, return an empty list for that answer.
+Your response should consist of lines in the format as such:
+[a comment's index] : [a list of indices of the mentioned criteria by the comment]
+
+\n{indexed_criteria_string}'''
+
+    print(prompt)
     return openai.ChatCompletion.create(
         messages=[
-            {"role": "system", "content": "You evaluate comments on an e-cigarette product against a list of criteria I provide you."},
-            {"role": "user", "content": prompt+indexed_criteria_string},
+            {"role": "system",
+             "content": "You evaluate comments on an e-cigarette product against a list of criteria I provide you."},
+            {"role": "user", "content": prompt},
         ],
         model="gpt-4",
-        temperature=0.1,
+        temperature=0,
         request_timeout=300,
     )
 
-# number to letter, e.g. 29---‘ad’
+
 def num_to_letter_index(num: int):
+    # number to letter, e.g. 29---‘ad’
+
     quotient = num // 26
     remainder = num % 26
     if quotient == 0:
-        return chr(97+num)
+        return chr(97 + num)
     else:
-        return chr(97+quotient-1)+chr(97+remainder)
+        return chr(97 + quotient - 1) + chr(97 + remainder)
 
-# letter to number
+
 def letter_index_to_num(ind: str):
+    # letter to number
+
     num = -1
     rev_ind = list(reversed(ind))
     for i in range(len(ind)):
-        num += (26**i)*(ord(rev_ind[i])-96)
+        num += (26 ** i) * (ord(rev_ind[i]) - 96)
     return num
 
-def insert_response_into_target_matrix(response, outmatrix, num_of_comments):
-    # Intelligently integrate GPT response into a target matrix
 
+def insert_response_into_target_matrix(response, outmatrix, comment_indices):
+    # Intelligently integrate GPT response into a target matrix
+    num_of_comments = len(comment_indices)
     list_of_resp = (
         (response["choices"][0]["message"]["content"])
         .strip("\n")
@@ -94,8 +102,10 @@ def insert_response_into_target_matrix(response, outmatrix, num_of_comments):
         print(len(list_of_resp), " responses")
         print(num_of_comments, " comments")
     for resp in list_of_resp:
-        mentioned_criteria_indices = np.array(re.findall("\d+", resp), dtype="i")
-        comment_index = letter_index_to_num(re.findall("[a-z]+",resp.split("***")[0])[0])
+        comment_index_split = resp.split(":")[0]
+        criteria_index_split = resp.split(":")[1]
+        comment_index = letter_index_to_num(re.findall("[a-z]+", comment_index_split)[0])
+        mentioned_criteria_indices = np.array(re.findall("\d+", criteria_index_split), dtype="i")
         outmatrix.loc[comment_index, :] = 0
         outmatrix.loc[comment_index, mentioned_criteria_indices] = 1
     return outmatrix.copy()
@@ -125,36 +135,34 @@ def divide_into_batches(data, length_of_batch):
         print("successfully read criteria. You opted not to batch criteria.")
     return batch_list
 
-def comment_labeling_with_gpt(m_comments, m_criteria, criteria_batch_size=0, comments_batch_size=50):
+
+def comment_labeling_with_gpt(batched_comments, batched_criteria, criteria_batch_size, comments_batch_size):
     # comments: a Series of comments.
     # criteria: a Series of criteria
 
     # 初始回答汇总列表, concatenate later
     all_answers = []
 
-    # batch process criteria and comments
-    batched_criteria = divide_into_batches(m_criteria, criteria_batch_size)
-    batched_comments = divide_into_batches(m_comments, comments_batch_size)
-
     counter = 1
     try:
         for c in range(len(batched_comments)):
             this_comments = batched_comments[c]
             # initialize the output matrix with np.nan
-            answers = pd.DataFrame(columns=m_criteria.index, index=this_comments.index)
             for j in range(len(batched_criteria)):
                 # 将每个问题上传给ChatGPT并获取回答
                 this_criteria = batched_criteria[j]
+                answers = pd.DataFrame(columns=this_criteria.index, index=this_comments.index)
                 response = get_response_from_gpt(this_comments, this_criteria)
                 print(response)
                 print("successfully responded")
-                insert_response_into_target_matrix(response, answers, len(this_comments))
+                insert_response_into_target_matrix(response, answers, this_comments.index)
                 # comments that are missed by GPT will retrain np.nan and will later be reprocessed
                 print(
-                    f"{counter}; [{this_comments.index[0]}, {this_comments.index[-1]}] comment; [{this_criteria.index[0]}, {this_criteria.index[-1]}] criteria; {datetime.now()}"
+                    f"{counter}; [{this_comments.index[0]}, {this_comments.index[-1]}] comment; "
+                    f"[{this_criteria.index[0]}, {this_criteria.index[-1]}] criteria; {datetime.now()}"
                 )  # counter提示程序正常运行
                 counter += 1
-            all_answers.append(answers.copy())
+                yield answers
     except Exception as err:
         # 如果出错，保存现有的回答，防止已有进度丢失
         print(err)
@@ -162,16 +170,19 @@ def comment_labeling_with_gpt(m_comments, m_criteria, criteria_batch_size=0, com
         concated_ans = pd.concat(all_answers)
         print(len(concated_ans), 'answers returned from ', len(m_comments), 'comments received')
         return concated_ans
+
+
 #         all_answers_df = pd.concat(all_answers)
 #         return all_answers_df.applymap(
 #             lambda x: x.strip("\n") if type(x) == str else x
 #         )  # strip the extra \n
 
 
-def main(comment_file_name, read_dir, save_dir, criteria_file_dir, api_key='', criteria_batch_size=0, comments_batch_size=100):
+def main(comment_file_name, read_dir, save_dir, criteria_file_dir, api_key='', criteria_batch_size=0,
+         comments_batch_size=100):
     # criteria_batch_size/comment_batch_size: 批次处理中一批的容量大小，0表示不分批
 
-
+    # 初始化计时器，读取各种文件
     start = time.time()
     g_criteria = pd.read_csv(criteria_file_dir, index_col=0)["criterion"]
     save_path = os.path.join(save_dir, comment_file_name)
@@ -181,26 +192,32 @@ def main(comment_file_name, read_dir, save_dir, criteria_file_dir, api_key='', c
     if os.path.isfile(save_path):
         old_results = pd.read_csv(save_path, index_col=0)
         if len(old_results) == 0:
-            last_comment = 0
+            last_processed_comment_index = 0
         else:
-            last_comment = len(old_results)
+            last_processed_comment_index = len(old_results)
     else:
         pd.DataFrame(columns=g_criteria).to_csv(save_path)
-        last_comment = 0
+        last_processed_comment_index = 0
 
     # Continue reading comments from the last one unprocessed
-    g_comments = pd.read_csv(read_path, index_col=0)['comment'].loc[last_comment:]
+    g_comments = pd.read_csv(read_path, index_col=0)['comment'].loc[last_processed_comment_index:]
 
-#    openai.api_key = api_key  # ChatGPT密匙
+    # batch process criteria and comments
+    batched_criteria = divide_into_batches(g_criteria, criteria_batch_size)
+    batched_comments = divide_into_batches(g_comments, comments_batch_size)
+
+    # openai.api_key = api_key  # ChatGPT密匙
     new_results = comment_labeling_with_gpt(g_comments, g_criteria,
                                             criteria_batch_size=criteria_batch_size,
                                             comments_batch_size=comments_batch_size)
+
+    # 检查无效值
     naindxs = new_results[new_results.isna().any(axis=1)].index
-    if len(naindxs) > 0:
+    if len(naindxs) > 0:  # 若有无效值，将对应评论重新处理
         print("nan values are found at ", naindxs.tolist())
         response = get_response_from_gpt(g_comments[naindxs], g_criteria)
         print(response)
-        new_results = insert_response_into_target_matrix(response, new_results, len(naindxs))
+        new_results = insert_response_into_target_matrix(response, new_results, naindxs)
     else:
         print("No nan values are found! All comments have been processed!")
     new_results.index = g_comments[new_results.index]
@@ -210,18 +227,20 @@ def main(comment_file_name, read_dir, save_dir, criteria_file_dir, api_key='', c
     )  # Print average processing time
 
 
-openai.api_key = '' # key
+openai.api_key = ''  # key
 
-# 运行语句
+# 运行
 if __name__ == '__main__':
-    logging.basicConfig(filename='mylog.log', encoding='utf-8', level=logging.INFO, filemode='a', format='%(asctime)s %(message)s')
+    logging.basicConfig(filename='mylog.log', encoding='utf-8', level=logging.INFO, filemode='a',
+                        format='%(asctime)s %(message)s')
+    # main(评论文件名，读取评论文件路径，保存结果路径，标签文件地址)
+    # api_key: 选填
+    # criteria_batch_size/comment_batch_size: 分批处理中一批次的容量大小，0表示不分批
     main(
         'weedmaps.csv',
-         'data',
-         'processed-results-2',
-         'label_and_questions.csv',
+        'data',
+        'processed-results',
+        'label_and_questions.csv',
         criteria_batch_size=0,
-        comments_batch_size=100,
+        comments_batch_size=90,
     )
-
-
